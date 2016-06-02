7da7df2fb732b5215c08ab8277859ec85b814dac
hermanzamula <herman.zamula@gmail.com>
2016-06-02 15:06:50 +0300

    Update Notifier.java

--- model/src/main/java/com/taxiservice/model/Notifier.java
+++ model/src/main/java/com/taxiservice/model/Notifier.java
@@ -1,18 +1,7 @@
-/*
- * C O P Y R I G H T   N O T I C E
- * -----------------------------------------------------------------------
- * Copyright (c) 2011-2012 InfoClinika, Inc. 5901 152nd Ave SE, Bellevue, WA 98006,
- * United States of America.  (425) 442-8058.  http://www.infoclinika.com.
- * All Rights Reserved.  Reproduction, adaptation, or translation without prior written permission of InfoClinika, Inc. is prohibited.
- * Unpublished--rights reserved under the copyright laws of the United States.  RESTRICTED RIGHTS LEGEND Use, duplication or disclosure by the
- */
 package com.taxiservice.model;
 
 import java.util.Date;
 
-/**
- * @author Stanislav Kurilin
- */
 public interface Notifier {
 
     void sendTaxiOrder(long actor, long driver, long city, Date onDate, String message);
